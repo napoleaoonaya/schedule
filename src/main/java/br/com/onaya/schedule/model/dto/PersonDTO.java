4272ed4f1fdeb258d376dac3b3041140bdcf97fb
napoleaoonaya <napoleao.onaya@gmail.com>
2023-01-01 00:39:04 -0300

    Removing attributes

--- src/main/java/br/com/onaya/schedule/model/dto/PersonDTO.java
+++ src/main/java/br/com/onaya/schedule/model/dto/PersonDTO.java
@@ -1,19 +1,8 @@
 package br.com.onaya.schedule.model.dto;
 
 import br.com.onaya.schedule.model.Person;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 public class PersonDTO extends Person{
-	//private Long id;
-    //private String name;
-    //private String middleName;
-    //private String lastName;
-    //private String address;
-    //private String email;
-    //private String cell;
-    //private String telephone;
-    //private Person person;
 }
